cb7662250ac69177b7b963ec8044bbe498a6ec0c
itsiporenko <iren.tsiporenko@gmail.com>
2024-10-17 21:13:07 +0300

    Add Delete Fix Edit issue

--- to-do-list/todolist/tasks/urls.py
+++ to-do-list/todolist/tasks/urls.py
@@ -5,7 +5,6 @@ from  . import views
 urlpatterns = [
 
     path('', views.index, name=""),
-
     path('edit_task/<str:pk>/', views.editTask, name="edit_task"),
-
+    path('delete_task/<str:pk>/', views.deleteTask, name="delete_task"),
 ]
